ca779b25d64e52125db2e6469d9fed2b72fff353
seeneedeen <6210110083@psu.ac.th>
2022-08-08 16:32:16 +0700

    5testcase

--- exercise1/code/FizzBuzz.py
+++ exercise1/code/FizzBuzz.py
@@ -1,7 +1,7 @@
-def it_FizzorBuzz(numbers):
-    if numbers % 3 and numbers % 5 == 0:
-        return "FizzBuzz"
-    elif numbers % 3 == 0:
-        return "Fizz"
-    elif numbers % 5 == 0:
-        return "Buzz"
+def it_FizzorBuzz(numbers):     
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            return "FizzBuzz"
+        elif numbers % 3 == 0:
+            return "Fizz"
+        elif numbers % 5 == 0:
+            return "Buzz"
--- exercise1/tests/test_FizzBuzz.py
+++ exercise1/tests/test_FizzBuzz.py
@@ -7,3 +7,19 @@ class testFizzorBuzz(unittest.TestCase):
         number = 3
         it_Fizz = it_FizzorBuzz(number)
         self.assertEqual(it_Fizz,"Fizz")
+    
+    def test_give_5(self):
+        number = 5
+        it_Buzz = it_FizzorBuzz(number)
+        self.assertEqual(it_Buzz,"Buzz")    
+
+    def test_give_15(self):
+        number = 15
+        it_Fizz = it_FizzorBuzz(number)
+        self.assertEqual(it_Fizz,"FizzBuzz")
+    
+    def test_give_15(self):
+        number = 15
+        it_Fizz = it_FizzorBuzz(number)
+        self.assertEqual(it_Fizz,"FizzBuzz")
+        
