3a7be13ab45f1fd934d504027bdcd81266a790a9
seeneedeen <6210110083@psu.ac.th>
2022-08-14 21:59:35 +0700

    addunittest

--- exercise1/code/FizzBuzz.py
+++ exercise1/code/FizzBuzz.py
@@ -1,7 +1,10 @@
-def it_FizzorBuzz(numbers):     
+def it_FizzorBuzz(numbers):
+    if numbers >= 1 and numbers <= 1000:
         if numbers % 3 == 0 and numbers % 5 == 0:
             return "FizzBuzz"
         elif numbers % 3 == 0:
             return "Fizz"
         elif numbers % 5 == 0:
             return "Buzz"
+    else:
+        return "outofrange"
--- exercise1/tests/test_FizzBuzz.py
+++ exercise1/tests/test_FizzBuzz.py
@@ -18,8 +18,17 @@ class testFizzorBuzz(unittest.TestCase):
         it_Fizz = it_FizzorBuzz(number)
         self.assertEqual(it_Fizz,"FizzBuzz")
     
-    def test_give_15(self):
-        number = 15
+    def test_give_20(self):
+        number = 20
         it_Fizz = it_FizzorBuzz(number)
-        self.assertEqual(it_Fizz,"FizzBuzz")
-        
+        self.assertEqual(it_Fizz,"Buzz")
+
+    def test_give_negative1(self):
+        number = -1
+        it_Fizz = it_FizzorBuzz(number)
+        self.assertEqual(it_Fizz,"outofrange") 
+    
+    def test_give_1001(self):
+        number = 1001
+        it_Fizz = it_FizzorBuzz(number)
+        self.assertEqual(it_Fizz,"outofrange") 
